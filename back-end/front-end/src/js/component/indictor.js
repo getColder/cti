131a863f19cc8b5734120ab3d8ab760d7ae35800
Jiff <597368048@qq.com>
2021-12-13 16:57:18 +0800

    led

--- back-end/front-end/src/js/component/indictor.js
+++ back-end/front-end/src/js/component/indictor.js
@@ -9,13 +9,13 @@ const indictor = {
             </tr>\
             <tr id="waterHighLevel">\
                 <td style="font-size: 25px">高水位</td>\
-                <td><div class="waterHighLevel" v-if="ledindic[0]"></div><img :src="(ledindic[0]!==undefined)?(ledindic[0]==1)?indic[1]:indic[0]:indic[0]" width="25px" height="25px"></td>\
-                <td><div class="waterHighLevel" v-if="ledindic[1]"></div><img :src="(ledindic[1]!==undefined)?((ledindic[1]==1)?indic[1]:indic[0]):indic[0]" width="25px" height="25px"></td>\
+                <td><div class="waterHighLevel" v-if="ledindic[0] == 1"></div><img :src="(ledindic[0]!==undefined)?(ledindic[0]==1)?indic[1]:indic[0]:indic[0]" width="25px" height="25px"></td>\
+                <td><div class="waterHighLevel" v-if="ledindic[1] == 1"></div><img :src="(ledindic[1]!==undefined)?((ledindic[1]==1)?indic[1]:indic[0]):indic[0]" width="25px" height="25px"></td>\
             </tr>\
             <tr>\
                 <td style="font-size: 25px">低水位</td>\
-                <td><div class="waterLowLevel" v-if="!ledindic[0]"></div><img :src="(ledindic[0]!==undefined)?((ledindic[0]==0)?indic[2]:indic[0]):indic[0]" width="25px" height="25px"></td>\
-                <td><div class="waterLowLevel" v-if="!ledindic[1]"></div><img :src="(ledindic[1]!==undefined)?((ledindic[1]==0)?indic[2]:indic[0]):indic[0]" width="25px" height="25px"></td>\
+                <td><div class="waterLowLevel" v-if="ledindic[0] == 0"></div><img :src="(ledindic[0]!==undefined)?((ledindic[0]==0)?indic[2]:indic[0]):indic[0]" width="25px" height="25px"></td>\
+                <td><div class="waterLowLevel" v-if="ledindic[1] == 0"></div><img :src="(ledindic[1]!==undefined)?((ledindic[1]==0)?indic[2]:indic[0]):indic[0]" width="25px" height="25px"></td>\
             </tr>\
         </tbody>\
     </table>',
