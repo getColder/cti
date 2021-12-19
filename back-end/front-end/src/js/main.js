21141d5ac9b54626313c6a7d529bc3902a07b310
Jiff <597368048@qq.com>
2021-12-19 23:24:56 +0800

    devlist

--- back-end/front-end/src/js/main.js
+++ back-end/front-end/src/js/main.js
@@ -278,6 +278,7 @@ var infoListBox = new Vue({
                     return this.timenodes;
                 case 1:
                     this.title = '设备列表'
+                    console.log(this.devnodes)
                     return this.devnodes;
                 case 2:
                     this.title = '数据库查询结果'
