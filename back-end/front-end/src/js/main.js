bfb922b357d7513fb3532adc085802a850f3ac38
Jiff <597368048@qq.com>
2021-12-19 22:46:29 +0800

    devlist

--- back-end/front-end/src/js/main.js
+++ back-end/front-end/src/js/main.js
@@ -238,6 +238,8 @@ var infoListBox = new Vue({
                         }
                         devicesList = that.devnodes = devs;
                         devicesOnlineList = response.data.on;
+                        console.log(devicesOnlineList)
+                        console.log(response.data)
                     }
                     else
                     devicesList = that.devnodes = [];
