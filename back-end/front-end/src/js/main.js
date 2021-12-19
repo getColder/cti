8e0c2ef39fee0626defa7023494ee15045e25144
Jiff <597368048@qq.com>
2021-12-19 19:12:33 +0800

    devlist

--- back-end/front-end/src/js/main.js
+++ back-end/front-end/src/js/main.js
@@ -228,7 +228,7 @@ var infoListBox = new Vue({
                     if(response.data){
                         var devs = response.data.all;
                         for (let index = 0; index < devs.length; index++) {
-                            dev = dev.substring(4, dev.length);                 
+                            dev[index] = dev[index].substring(4, dev.length);                 
                         }
                         that.devnodes = devs; 
 
