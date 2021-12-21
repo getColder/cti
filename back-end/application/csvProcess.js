2814413ef8c8b60f3ddcb036a8701d93a4dca9ff
jiff <597368048@qq.com>
2021-12-22 00:40:02 +0800

    fixed csv-download

--- back-end/application/csvProcess.js
+++ back-end/application/csvProcess.js
@@ -31,17 +31,18 @@ function makeRow(rowObj, row) {
 async function csvMake(dataObjArry) {
     var head = 'timeStap,floor,temp entry,temp out,vavle state,cool rotation,back rotation,\
   temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp envir\r\n';
-    if(dataObjArry === null || dataObjArry === undefined){
-        return head;
-    }
-    if(dataObjArry === []){
-        return head;
-    }
-    if(dataObjArry === {}){
-        return head;
-    }
+    console.log(dataObjArry)
     for (index in dataObjArry) {
         var data = dataObjArry[index];
+        if(data === null || data === undefined){
+            return head;
+        }
+        if(data === []){
+            return head;
+        }
+        if(data === {}){
+            return head;
+        }
         if (!checkInfo(data)) {
             console.log('返回前' + index + '条数据.')
             return head;
